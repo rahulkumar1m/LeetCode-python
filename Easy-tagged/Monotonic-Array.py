c5a9da5231c82ff52b46521cd8e3a86d30adede1
Rahul <32988067+rahulkumar1m@users.noreply.github.com>
2021-04-03 19:31:42 +0530

    +Problem Statement

--- Easy-tagged/Monotonic-Array.py
+++ Easy-tagged/Monotonic-Array.py
@@ -1,6 +1,13 @@
 # Time: O(n)
 # Space: O(1)
 
+
+# An array is monotonic if it is either monotone increasing or monotone decreasing.
+
+# An array A is monotone increasing if for all i <= j, A[i] <= A[j].  An array A is monotone decreasing if for all i <= j, A[i] >= A[j].
+
+# Return true if and only if the given array A is monotonic.
+
 class Solution:
     def isMonotonic(A: list) -> bool:
         incr = decr = True
