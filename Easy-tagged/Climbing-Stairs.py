9e7fec2e8ce3a9cee52a604c46c3f3fe68edbe62
Rahul <32988067+rahulkumar1m@users.noreply.github.com>
2021-04-03 23:01:08 +0530

    Solution - Climbing Stairs

---
+++ Easy-tagged/Climbing-Stairs.py
@@ -0,0 +1,21 @@
+# Time: O(1)
+# Space: O(1)
+
+
+# You are climbing a staircase. It takes n steps to reach the top.
+
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+# This problem is just a subset of finding n+1 th number in fibonacci series
+
+from math import sqrt
+
+class Solution:
+    def __init__(self):
+        self.sqrt_5 = sqrt(5)
+        self.l1 = (1 + self.sqrt_5)/2
+        self.l2 = (1 - self.sqrt_5)/2
+        
+    def climbStairs(self, n: int) -> int:
+        n += 1
+        return int((self.l1**n-self.l2**n)/self.sqrt_5)
