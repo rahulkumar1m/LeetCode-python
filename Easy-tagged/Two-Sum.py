66a59dd2ee9d12fa0cc684f61087d6c62402bb5d
Rahul <32988067+rahulkumar1m@users.noreply.github.com>
2021-04-08 19:23:26 +0530

    Solution2: Two Sum Time: O(N^2)
    The time complexity is ^2 beacuse "x in list" takes O(N) and we
    are already iterating a for loop. Hence quadtratic time complex

--- Easy-tagged/Two-Sum.py
+++ Easy-tagged/Two-Sum.py
@@ -10,9 +10,9 @@
 
 # You can return the answer in any order.
 
-class Solution:
+
+class Solution1:
     def twoSum(self, nums, target: int):
-        h = {}
         # Looping through all elements in the list to check for 
         for i in range(len(nums)-1):
             for j in range(i+1, len(nums)):
@@ -21,3 +21,15 @@ class Solution:
         
         return []
 
+class Solution2:
+    def twoSum(self, nums, target: int):
+        h = {}
+        # Looping through all elements in the list to check for 
+        for i, num in enumerate(nums):
+            n = target - num
+            if n not in h:
+                h[num] = i
+            else:
+                return [h[num], i]
+        
+        return []
